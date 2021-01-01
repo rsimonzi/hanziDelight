d6cd17d889faa9c4c5e21bea981739d369cc18a4
rsimonzi <rsimonzi@hotmail.com>
2020-12-31 22:40:02 -0800

    adding landing page

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3012, function(req, res){
+  console.log("Server running on port 3012");
+})
